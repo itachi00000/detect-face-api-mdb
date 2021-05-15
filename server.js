103c55f0d4a015f44c20fba422d0f75511e8fc2b
itachi0 <>
2021-05-15 21:27:27 +0800

    uncomment passport, exp-session, edit some code in server

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const express = require('express');
 const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
+const expSession = require('express-session');
+// const mongoSessionStore = require('connect-mongo');
+// const passport = require('passport');
 const morgan = require('morgan');
 const cors = require('cors');
 // const helmet = require('helmet');
+const path = require('path');
 
+// some var, import, utils
 const { connectMDB } = require('./db');
 const { errorHandler } = require('./utils/error-handler.utils');
 
@@ -11,7 +17,9 @@ const { errorHandler } = require('./utils/error-handler.utils');
 dotenv.config({ path: './.env.dev.local' });
 
 // connect mongodb + more .catch()
-connectMDB().catch((err) => console.error(err.stack()));
+connectMDB(mongoose).catch((err) =>
+  console.error('Catch-Error-MDNconnect', err.stack)
+);
 
 // init express app
 const app = express();
@@ -20,18 +28,49 @@ const app = express();
 const usersRoute = require('./routes/user.route');
 
 // setting vars, options, env
-const PORT = parseInt(process.env.PORT || 3000, 10);
-const NODE_ENV = process.env.NODE_ENV || 'none';
+const port = parseInt(process.env.PORT || 3000, 10);
+const ROOT_URL = `http://localhost:${port}`;
+const dev = process.env.NODE_ENV !== 'production';
+// const MongoStore = mongoSessionStore(expSession);
+// const sessOpts = {
+//   name: process.env.SESSION_NAME,
+//   secret: process.env.SESSION_SECRET,
+//   store: new MongoStore({
+//     mongooseConnection: mongoose.connection, // based on connection?
+//     ttl: 14 * 24 * 60 * 60 // save session 14days
+//   }),
+//   resave: false,
+//   saveUninitialized: false,
+//   cookie: {
+//     httpOnly: true, // default?
+//     maxAge: 14 * 24 * 60 * 60 * 1000 // ms??
+//   }
+// };
 
-// 3rd mwares
+// 3rd mwares ----------
 app.use(cors());
 // app.use(helmet())
-app.use(morgan('dev'));
+if (dev) {
+  app.use(morgan('dev'));
+}
 app.use(express.json()); // req.body
 app.use(express.urlencoded({ extended: true }));
+// app.use(expSession(sessOpts));
+// passport
+// app.use(passport.initialize());
+// app.use(passport.session());
+
+// passport.serializeUser((user, done) => {
+//   done(null, user);
+// });
+
+// passport.deserializeUser((user, done) => {
+//   done(null, user);
+// });
 
 // mwares
-app.use(express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
 
 // routes
 app.use('/', usersRoute);
@@ -48,7 +87,13 @@ app.use('*', (req, res, next) => {
 app.use(errorHandler);
 
 //listen
-app.listen(PORT, (err) => {
-  if (err) throw err;
-  console.log(`<< Exp-Server running @ http://127.0.0.1:${PORT}/`);
+app.listen(port, (err) => {
+  if (err) {
+    console.error('error @ app.listen-', err);
+    throw err;
+  }
+  console.log(
+    `<< Exp-Server (${process.env.NODE_ENV ||
+      'no-env'}) is Ready on ${ROOT_URL}!`
+  );
 });
